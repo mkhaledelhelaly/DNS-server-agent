0cc67bbb6b38c765b327cf86d242988da3504582
AliSherif107 <Alisherif142@gmail.com>
2024-12-08 20:40:06 +0200

    aaa

--- dns_resolver.py
+++ dns_resolver.py
@@ -6,7 +6,7 @@ resolver_port = 53
 root_ip = '127.0.0.1'
 root_port = 5354
 tld_ip = '127.0.0.1'
-tld_port = 5354
+tld_port = 5355
 auth_ip = '127.0.0.1'
 auth_port = 5355
 
@@ -43,31 +43,31 @@ while True:
     print(f"Binary Domain Name = {binary_domain_name}")
     print(f"Resolver received data from client: {data}")
     
-    
+    #ROOT SERVER
     # Step 2: Send data to the root server
     sock.sendto(data, (root_ip, root_port))
     print(f"Resolver sent data {data} to root server")
-
     # Step 3: Receive response from the root server
     root_response, _ = sock.recvfrom(512)
     print(f"Resolver received response from root: {root_response}")
     break
 
+    #TLD SERVER
     # Step 4: Send response to the TLD server
     sock.sendto(root_response, (tld_ip, tld_port))
     print(f"Resolver sent data to TLD server")
-
-    # # Step 5: Receive response from the TLD server
+     # # Step 5: Receive response from the TLD server
     # tld_response, _ = sock.recvfrom(512)
     # print(f"Resolver received response from TLD: {tld_response}")
 
+    #AUTHORITATIVE SERVER
     # # Step 6: Send response to the authoritative server
     # sock.sendto(tld_response, (auth_ip, auth_port))
     # print(f"Resolver sent data to authoritative server")
-
     # # Step 7: Receive response from the authoritative server
     # auth_response, _ = sock.recvfrom(512)
     # print(f"Resolver received response from authoritative: {auth_response}")
+
 
     # # Step 8: Send the final response to the client
     # sock.sendto(auth_response, addr)
